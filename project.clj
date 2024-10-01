3a3318e081a5f8fb2ec4e3148527f4af34d8f8be
Iñaki Arenaza <inaki.arenaza@biotz.io>
2024-10-01 13:03:50 +0200

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/rbac.next "0.1.0-alpha-6"
+(defproject dev.gethop/rbac.next "0.1.0-alpha-7-SNAPSHOT"
   :description "A Clojure library designed to provide role-based access control (RBAC)"
   :url "https://github.com/gethop-dev/rbac"
   :license {:name "Mozilla Public Licence 2.0"
